6a8fcfd334387d0a8730bff55162985d6307b9f4
hellomaryjo <majorx3music@gmail.com>
2023-01-27 15:31:41 -0600

    testing one quote, updating char names to secret name

--- index.js
+++ index.js
@@ -7,26 +7,57 @@ export const getFilmIds = async () => {
   const data = await response.text();
   const $ = cheerio.load(data);
   let idsArr = []
-  $(".titleColumn a").each((_, el) => {
+  $(".titleColumn a").each((index, el) => {
     let extractMovId = /(?<=title\/)(.*)(?=\/)/gm;
     let movId = $(el).attr("href").match(extractMovId)[0];
-    idsArr.push(movId);
+
+    if (index < 1) {
+      idsArr.push(movId);
+    }
   })
+  console.log(idsArr);
   return idsArr;
 }
 
 const getQuotes = async () => {
   const filmIds = await getFilmIds();
-  const requests = await filmIds.map(async id => {
+  const requests = await filmIds.map(async (id, index) => {
     const urls = await fetch(`https://www.imdb.com/title/${id}/quotes`);
     const html = await urls.text();
     const $ = cheerio.load(html);
+
+    let quoteArr = [];
+
+    $(".quote .sodatext").each((_, el) => {
+      // character
+      const character = $(el).find(".character");
+      // subtext lines
+      const fine = $(el).find(".fine").text();
+      // char arr
+      let charArr = [];
+      // new text
+      let text = $(el).text().replaceAll(`[${fine}]`, "");
+
+      $(character).each((_, charEl) => {
+        charArr = [...charArr, $(charEl).text()];
+      });
+
+      // replace char names with Char + index
+      // TODO: can move this outside of the fn so it removes ALL ref to chars
+      $([...new Set(charArr)]).each((index, item) => {
+        text = text.replaceAll(item, `Character ${index + 1}`);
+      });
+
+      quoteArr = [...quoteArr, text];
+    });
+
     return {
-      urls,
-      quote: $(".quote .sodatext p")
+      // urls, do I need this?
+      id: index,
+      quotes: quoteArr
     }
   })
   return Promise.all(requests);
 }
 
-getQuotes().then(val => console.log(val));
+getQuotes().then(val => console.log(val[0].quotes[2]));
